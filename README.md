b3f4efb14901dfef195f140c61922c60e5b4007c
Lierexa <noahtueremis@gmail.com>
2024-10-14 09:20:56 +0200

    Basic Configuration of the Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Projekt Homelab
  Kleiner Home Server basierend auf einem Raspberry Pi.
+ OS: Ubuntu Server 24.04 LTS
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Homeserver Setup script v1.0"
+
+# Update package list and upgrade packages
+
+sudo apt upgrade && apt update -y
+
+# Installing Prerequisites, Docker Engine & Portainer
+
+sudo apt install curl -y
+
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Create Docker Volume for portainer
+
+docker volume create portainer_data
+
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:2.21.3
