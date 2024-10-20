e753a2aab322cd922108c42401ed83e9c2ee0a7b
Lierexa <noahtueremis@gmail.com>
2024-10-20 14:07:41 +0200

    updated repo

--- setup.sh
+++ setup.sh
@@ -1,93 +1,45 @@
-#!/bin/sh
-echo "Homeserver Setup script v1.0"
-echo "----------------------------"
+#!/bin/bash
+
+echo "Home server setup script v1.1"
+echo "-----------------------------"
 echo ""
-echo "Installing System Updates."
+
+echo "Installing system updates."
 echo ""
 
 sudo apt update
 sudo apt upgrade -y
 sudo do-release-upgrade
+echo ""
 
-echo "Installing needed apps and dependencies."
+echo "Install needed packages."
+echo ""
+sudo apt install curl git fail2ban ca-certificates -y
+echo ""
 
-sudo apt install curl ca-certificates -y
+echo "Change timezone to Europe/Berlin."
+sudo timedatectl set-timezone Europe/Berlin
+echo ""
 
-echo "Adding dependencies for docker."
-
-sudo install -m 0755 -d /etc/apt/keyrings
+echo "Installing docker engine."
+echo ""
+echo "Adding dockers GPG key."
+echo ""
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
+echo ""
+echo "Adding the repo to apt sources."
+echo ""
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "Installing docker engine."
+sudo apt update
+echo ""
+echo "Installing the docker packages."
+echo ""
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo ""
 
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-echo "Creating docker volume for Portainer."
-
-docker volume create portainer_data
-
-docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
-
-echo "Setting up file structure in /opt/ for docker."
-
-mkdir /opt/Docker
-
-echo "Creating docker Container for homepage."
-
-docker run -d --name homepage \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -p 80:3000 \
-  -v /opt/Docker/homepage/config:/app/config \
-  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  --restart unless-stopped \
-  ghcr.io/gethomepage/homepage:latest
-
-echo "Creating docker container for NetBox"
-
-git clone -b release https://github.com/noahtrr/netbox-docker.git #Customized for restart policies, nothing else.
-cd netbox-docker
-docker compose pull
-docker compose up -d
-
-echo "Return to previous directory."
-
-cd ..
-
-echo "Create your_spotify container. "
-
-git clone -b main https://github.com/noahtrr/Projekt-Homelab.git
-
-cd Projekt-Homelab/Docker-Containers/your_spotify
-
-docker compose up -d
-
-echo "Return to docker container path.."
-
-cd ..
-
-echo "Creating InfluxDB container."
-
-cd influxdb
-
-docker compose up -d
-
-echo "Return to home directory."
-
-cd ..
-cd ..
-
-echo "Creating Grafana docker container."
-
-docker run -d -p 3000:3000 --name=grafana \
-  --volume grafana-storage:/var/lib/grafana \
-  --restart unless-stopped \
-  grafana/grafana-enterprise
+echo "Setting up docker containers."
